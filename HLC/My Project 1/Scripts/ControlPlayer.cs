679127a5c611fdfed8cde370aeb518dd9f8f397a
JLandero19 <118732003+JLandero19@users.noreply.github.com>
2024-10-14 14:37:58 +0200

    add Scripts

---
+++ HLC/My Project 1/Scripts/ControlPlayer.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ControlPlayer : MonoBehaviour
+{
+    public int fuerzaSalto;
+    public int velocidad;
+    private Rigidbody2D fisicas;
+    private SpriteRenderer sprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Inicializamos el componentes de las físicas
+        fisicas = GetComponent<Rigidbody2D>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
+        sprite.flipX = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Espera que apulsemos el botón espacio y TocandoSuelo() nos permite controlar el salto infinito
+        if (Input.GetKeyDown(KeyCode.Space) && TocandoSuelo())
+        {
+            fisicas.AddForce(Vector2.up * fuerzaSalto, ForceMode2D.Impulse);
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        float entradaX = Input.GetAxis("Horizontal");
+
+        if (fisicas.velocity.x > 0)
+        {
+            sprite.flipX = false;
+        } else
+        {
+            sprite.flipX = true;
+        }
+
+        fisicas.velocity = new Vector2(entradaX * velocidad, fisicas.velocity.y);
+    }
+
+    private bool TocandoSuelo()
+    {
+        // Esto lo que hace mirar si está tocando el suelo o no
+        RaycastHit2D toca = Physics2D.Raycast(transform.position + new Vector3(0, -1.6f, 0), Vector2.down, 0.2f);
+        return toca.collider != null;
+    }
+
+    internal void FinJuego()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+}
