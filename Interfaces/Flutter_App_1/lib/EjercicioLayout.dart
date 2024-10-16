3caa546e16fd82d95726fd9abb26cd8c1c58b7c4
JLandero19 <118732003+JLandero19@users.noreply.github.com>
2024-10-16 14:21:16 +0200

    add Flutter_App_1

---
+++ Interfaces/Flutter_App_1/lib/EjercicioLayout.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class EjercicioLayout extends StatefulWidget {
+  const EjercicioLayout({super.key});
+
+  @override
+  State<EjercicioLayout> createState() => _EjercicioLayoutState();
+}
+
+class _EjercicioLayoutState extends State<EjercicioLayout> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.green,
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Container(
+                width: 100,
+                height: double.infinity,
+                color: Colors.red,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.orange,
+                  ),
+                ],
+              ),
+              Container(
+                width: 100,
+                height: double.infinity,
+                color: Colors.blue,
+              ),
+            ],
+          ),
+        )
+    );
+  }
+}
